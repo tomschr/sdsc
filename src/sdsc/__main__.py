c8b24aff9b2d36908d9195a46e3f9911e651f837
Thomas Schraitle <toms@suse.de>
2017-01-19 14:21:56 +0100

    Add copyright notice

--- src/sdsc/__main__.py
+++ src/sdsc/__main__.py
@@ -1,6 +1,23 @@
-"""
-Entrypoint module, in case you use `python -msdsc`.
+#
+# Copyright (c) 2017 SUSE Linux GmbH
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of version 3 of the GNU General Public License as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, contact SUSE LLC.
+#
+# To contact SUSE about this file by physical or electronic mail,
+# you may find current contact information at www.suse.com
 
+"""
+Entrypoint module, in case you use `python -m sdsc`.
 
 Why does this file exist, and why __main__? For more info, read:
 
--- src/sdsc/cli.py
+++ src/sdsc/cli.py
@@ -1,7 +1,22 @@
-"""
-Module that contains the command line app.
+#
+# Copyright (c) 2017 SUSE Linux GmbH
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of version 3 of the GNU General Public License as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, contact SUSE LLC.
+#
+# To contact SUSE about this file by physical or electronic mail,
+# you may find current contact information at www.suse.com
 
-Why does this file exist, and why not put this in __main__?
+"""
 
   You might be tempted to import things from __main__ later, but that will cause
   problems: the code will get executed twice:
