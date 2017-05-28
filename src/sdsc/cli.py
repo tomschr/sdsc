41fad7e025b8c0bf669d4f47f64d2c9558277b55
Thomas Schraitle <tom_schr@web.de>
2017-05-28 21:52:29 +0200

    Support multiprocessing
    * Implement in sdsc.stylecheck as Pool
    * Add sdsc.common for constants
    * Revise sdsc.cli
    * Remove obsolete sdsc.log
    * Import from .common instead from multiprocessing
    * Fix testcase

--- src/sdsc/cli.py
+++ src/sdsc/cli.py
@@ -16,71 +16,97 @@
 # To contact SUSE about this file by physical or electronic mail,
 # you may find current contact information at www.suse.com
 
-"""
+"""Checks a given DocBook XML file for stylistic errors
+
 Usage:
-   sdsc [-h | --help]
-   sdsc  [-v ...] [options] INPUTFILE [OUTPUTFILE]
+    sdsc [-h | --help]
+    sdsc [-v ...] [options] XMLFILES...
 
-checks a given DocBook XML file for stylistic errors
-
-Positional Arguments:
-  INPUTFILE          DocBook XML file to check
-  OUTPUTFILE         optional result XML file; if not give, use stdout
+Required Arguments:
+    XMLFILES          Path to one or more XML files to style check
 
 Options:
-  -h, --help         show this help message and exit
-  --version          show version number and exit
-  -b, --bookmarklet  open Web page that lets you install a bookmarklet to
-                     manage style checker results
-  -s, --show         show final report in $BROWSER, or default browser if
-                     unset; not all browsers open report files correctly and
-                     for some users, a text editor will open; in such cases,
-                     set the BROWSER variable with: export BROWSER=/MY/BROWSER
-                     ; Chromium or Firefox will both do the right thing
-  --module           writes name of current check module to stdout
-  --performance      write performance measurements to stdout
-  --checkpatterns    check formal validity of built-in regular expression
-                     patterns
+    -h, --help        Shows this help
+    -v                Raise verbosity level
+    --version         Prints the version
+    -j <N>, --jobs=<N>
+                      Allow N jobs at once [default: {cpu}]
+    --output=<OUTFILE>, -o <OUTFILE>
+                      Optional file where results are written to
+    -s, --show        show final report in $BROWSER, or default browser if
+                      unset; not all browsers open report files correctly and
+                      for some users, a text editor will open; in such cases,
+                      set the BROWSER variable with: export BROWSER=/MY/BROWSER
+                      Chromium or Firefox will both do the right thing
+    --module          writes name of current check module to stdout
+    --checkpatterns   check formal validity of built-in regular expression
+                      patterns
 """
 
-#
-# Module that contains the command line app.
-#
-# Why does this file exist, and why not put this in __main__?
-#
-#  You might be tempted to import things from __main__ later, but that will cause
-#  problems: the code will get executed twice:
-#
-#  - When you run `python -msdsc` python will execute
-#    ``__main__.py`` as a script. That means there won't be any
-#    ``sdsc.__main__`` in ``sys.modules``.
-#  - When you import __main__ it will get executed again (as a module) because
-#    there's no ``sdsc.__main__`` in ``sys.modules``.
-#
-#  Also see (1) from http://click.pocoo.org/5/setuptools/#setuptools-integration
-#
 
-from docopt import docopt
+from docopt import docopt, DocoptExit
+import logging
+from logging.config import dictConfig
+from lxml import etree
+from multiprocessing import cpu_count
+
+
+from .common import (DEFAULT_LOGGING_DICT,
+                     CPU_FACTOR,
+                     errorcode, LOGLEVELS,
+                     )
+from .stylecheck import process
+
+
+#: Use __package__, not __name__ here to set overall logging level:
+log = logging.getLogger(__package__)
 
 
 def parsecli(cliargs=None):
+    """Parse CLI arguments with docopt
+
+    :param list cliargs: List of commandline arguments
+    :return: dictionary from docopt
+    :rtype: dict
+    """
     from sdsc import __version__
     version = "%s %s" % (__package__, __version__)
-    args = docopt(__doc__, argv=cliargs, version=version)
+    args = docopt(__doc__.format(cpu=cpu_count() * CPU_FACTOR),
+                  argv=cliargs, version=version)
+    dictConfig(DEFAULT_LOGGING_DICT)
+    log.setLevel(LOGLEVELS.get(args['-v'], logging.DEBUG))
+
+    log.debug("CLI result: %s", args)
     return args
+
+
+def checkargs(args):
+    """Check arguments
+
+    :param args: dictionary from docopt parsing
+    """
+    log.debug("Checking arguments")
+    try:
+        args['--jobs'] = int(args['--jobs']) * CPU_FACTOR
+    except ValueError:
+        raise DocoptExit("The --jobs option does not contain "
+                         "an integer value (got %r)" % args['--jobs'])
+    log.debug("Checking successful")
 
 
 def main(cliargs=None):
     """Entry point for the application script
 
     :param list cliargs: Arguments to parse or None (=use sys.argv)
-    :return: return zero or non-zero value
+    :return: return codes from ``ERROR_CODES``
     """
-    args = parsecli(cliargs)
-    app = App(args)
-    print(args)
-    print(app)
-    # print(app.xslcfiles)
-    print(app.parser)
-    app.run()
-    return 0
+
+    try:
+        args = parsecli(cliargs)
+        checkargs(args)
+        result = process(args)
+        log.info("Done.")
+        return result
+
+    except KeyboardInterrupt:
+        return 10
---
+++ src/sdsc/stylecheck.py
@@ -0,0 +1,92 @@
+#
+# Copyright (c) 2017 SUSE Linux GmbH
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of version 3 of the GNU General Public License as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, contact SUSE LLC.
+#
+# To contact SUSE about this file by physical or electronic mail,
+# you may find current contact information at www.suse.com
+
+import collections
+import concurrent.futures
+import itertools
+import logging
+from multiprocessing import Process, Pool, Queue, JoinableQueue, cpu_count
+import glob
+import random
+import time
+import os
+
+from .common import XSLCHECKPATH
+
+log = logging.getLogger(__name__)
+
+
+def getstylechecks():
+    """Return a list of all .xslc files
+
+    :return: style check files (.xslc)
+    """
+    return glob.glob(os.path.join(XSLCHECKPATH, "*.xslc"))
+
+
+def singlestylecheck(style, xmlfile):
+    """Perform a single stylecheck
+
+    :param data: tuple of styles and XML files
+    :return: tuple of a single entry; an entry contains XML file, style, and the result
+    """
+    # style, xmlfile = data
+    log.debug("Style check %r with %r", xmlfile, os.path.basename(style))
+    # Simulate a hard working function, sleep for 0..4s
+    sleep = random.randint(0, 20)/10
+    time.sleep(sleep)
+    log.debug("  stylesingle returns: %s", sleep)
+    return (xmlfile, style, sleep)
+
+
+def stylepool(xmlfiles, styles, jobs=None):
+    """Create a multiprocessing pool distributed on jobs
+
+    :param xmlfiles: list of XML files
+    :param styles: list of styles to apply to each XML file
+    :param jobs: integer to allow N jobs at once; None defaults to CPU count
+    :return: tuple of result
+    """
+    pool = Pool(jobs)
+    try:
+        values = pool.starmap(func=singlestylecheck,
+                              iterable=itertools.product(styles, xmlfiles))
+        pool.close() # close the process pool
+        return values
+    except KeyboardInterrupt:
+        log.error("Canceled.")
+        return None
+
+
+def process(args):
+    """Process the style check
+
+    :param dict args: Dictionary of parsed CLI arguments
+    :return: return exit value
+    :rtype: int
+    """
+    log.debug("Now in process()")
+    start = time.time()
+    results = stylepool(xmlfiles=args['XMLFILES'],
+                        styles=getstylechecks(),
+                        jobs=args['--jobs'])
+    end = time.time()
+
+    log.debug("process ended %.3fs", end - start)
+    log.debug("End result: %s", results)
+    return 0
