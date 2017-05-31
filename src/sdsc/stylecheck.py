1ac79ec448afb9f0f67a3122f9f32130687cd761
Thomas Schraitle <toms@suse.de>
2017-05-31 16:27:32 +0200

    Try to improve multiprocessing

--- src/sdsc/stylecheck.py
+++ src/sdsc/stylecheck.py
@@ -18,15 +18,27 @@
 
 import itertools
 import logging
-from multiprocessing import Pool
+from multiprocessing import Pool, Process, Queue, JoinableQueue, current_process
 import glob
 import random
 import time
 import os
+import signal
 
 from .common import XSLCHECKPATH
 
 log = logging.getLogger(__name__)
+
+
+
+class CleanExit(object):
+     def __enter__(self):
+             return self
+     def __exit__(self, exc_type, exc_value, exc_tb):
+         if exc_type is KeyboardInterrupt:
+             return True
+         return exc_type is None
+
 
 
 def getstylechecks():
@@ -43,13 +55,63 @@ def singlestylecheck(style, xmlfile):
     :param data: tuple of styles and XML files
     :return: tuple of a single entry; an entry contains XML file, style, and the result
     """
-    # style, xmlfile = data
-    log.debug("Style check %r with %r", xmlfile, os.path.basename(style))
+    jobname = current_process().name
+    log.info("%s %s", style, xmlfile)
+    log.debug("[%s] Style check %r with %r", jobname, xmlfile, os.path.basename(style))
     # Simulate a hard working function, sleep for 0..4s
     sleep = random.randint(0, 20)/10
     time.sleep(sleep)
-    log.debug("  stylesingle returns: %s", sleep)
     return (xmlfile, style, sleep)
+
+
+def stylepool_x(xmlfiles, styles, jobs=None):
+    """Create a multiprocessing pool distributed on jobs
+
+    :param xmlfiles: list of XML files
+    :param styles: list of styles to apply to each XML file
+    :param jobs: integer to allow N jobs at once; None defaults to CPU count
+    :return: tuple of result
+    """
+    #with Pool(jobs, init_worker) as pool:
+    #    try:
+    #        values = pool.starmap(func=singlestylecheck,
+    #                              iterable=itertools.product(styles, xmlfiles))
+    #        # pool.close() # close the process pool
+    #    except ExitProcessError:
+    #        log.error("Canceled.")
+    #        values = [None]
+    #    except KeyboardInterrupt:
+    #        log.fatal("Received ^C, aborting")
+    #        pool.terminate()
+    #        pool.join()
+    # ------------------------------------------
+    job_queue = Queue()
+    result_queue = Queue()
+
+    for data in itertools.product(styles, xmlfiles):
+        job_queue.put(data)
+
+    workers = []
+    for i in range(jobs):
+        p = Process(target=singlestylecheck,
+                    args=(i, job_queue, result_queue))
+        p.start()
+        workers.append(p)
+
+    try:
+        for process in workers:
+            process.join()
+    except KeyboardInterrupt:
+        log.error("Keyboard interrupt in stylepool")
+        for process in workers:
+            process.terminate()
+            process.join()
+        raise
+
+    values = []
+    while not result_queue.empty():
+        values.append(result_queue.get(block=False))
+    return values
 
 
 def stylepool(xmlfiles, styles, jobs=None):
@@ -60,15 +122,23 @@ def stylepool(xmlfiles, styles, jobs=None):
     :param jobs: integer to allow N jobs at once; None defaults to CPU count
     :return: tuple of result
     """
-    pool = Pool(jobs)
-    try:
-        values = pool.starmap(func=singlestylecheck,
-                              iterable=itertools.product(styles, xmlfiles))
-        pool.close() # close the process pool
-        return values
-    except KeyboardInterrupt:
-        log.error("Canceled.")
-        return None
+    def init_worker():
+        # Catch all ^C from child processes
+        signal.signal(signal.SIGINT, signal.SIG_IGN)
+
+    with Pool(jobs, init_worker) as pool:
+        try:
+            values = pool.starmap(func=singlestylecheck,
+                                  iterable=itertools.product(styles, xmlfiles))
+            pool.close()
+        except KeyboardInterrupt:
+            log.fatal("Received ^C, aborting")
+            # Not sure how to avoid that:
+            pool.terminate()
+            pool.join()
+            values = None
+
+    return values
 
 
 def process(args):
@@ -85,6 +155,6 @@ def process(args):
                         jobs=args['--jobs'])
     end = time.time()
 
-    log.debug("process ended %.3fs", end - start)
-    log.debug("End result: %s", results)
+    log.info("process ended %.3fs", end - start)
+    log.info("End result: %s", results)
     return 0
